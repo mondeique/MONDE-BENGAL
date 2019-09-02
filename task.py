71120f77d5e6bccba37404d7195891d7eb1deb38
ChanYeongShin <cyshin@yonsei.ac.kr>
2019-09-02 16:26:17 +0900

    [UPDATED] whosbag colorTag 지정

--- task.py
+++ task.py
@@ -136,8 +136,8 @@ def whosbag_web_crawling():
 # bagshoes_web_crawling()
 # mclanee_web_crawling()
 # mjade_web_crawling()
-pink_web_crawling()
-# whosbag_web_crawling()
+# pink_web_crawling()
+whosbag_web_crawling()
 
 
 

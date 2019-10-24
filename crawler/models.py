4229d66de3280a528f325b598dffa17ab049fc87
ChanYeongShin <cyshin@yonsei.ac.kr>
2019-10-24 10:15:38 +0900

    [UPDATED] convert image to RGB before save JPEG

--- crawler/models.py
+++ crawler/models.py
@@ -69,6 +69,7 @@ class BagImage(models.Model):
         # image = Image.open(BytesIO(resp.content))
         byteImgIO = BytesIO()
         byteImg = Image.open(BytesIO(resp.content))
+        byteImg = Image.Image.convert(mode="RGB", matrix=byteImg)
         byteImg.save(byteImgIO, "JPEG")
         byteImgIO.seek(0)
         byteImg = byteImgIO.read()

b75c02d38ef01063bc9532bdff7637f57090fd6c
ChanYeongShin <cyshin@yonsei.ac.kr>
2019-11-11 12:36:20 +0900

    [UPDATED] update task.py

--- task.py
+++ task.py
@@ -22,22 +22,28 @@ def crawler_2():
 
 
 def crawler_3():
-    gabangpop_web_crawling.delay()
+    # gabangpop_web_crawling.delay()
     pau_web_crawling.delay()
-    pink_web_crawling.delay()
+    # pink_web_crawling.delay()
     wconcept_web_crawling.delay()
-    whosbag_web_crawling.delay()
+    # whosbag_web_crawling.delay()
 
 # Auto run script at specific time
 import schedule
 import time
 
-schedule.every().monday.at("03:00").do(crawler_1)
-schedule.every().tuesday.at("03:00").do(crawler_2)
-schedule.every().wednesday.at("03:00").do(crawler_3)
-schedule.every().thursday.at("03:00").do(crawler_1)
-schedule.every().friday.at("03:00").do(crawler_2)
-schedule.every().saturday.at("03:00").do(crawler_3)
+schedule.every().monday.at("02:00").do(crawler_1)
+schedule.every().monday.at("06:00").do(crawler_1)
+schedule.every().tuesday.at("02:00").do(crawler_2)
+schedule.every().tuesday.at("06:00").do(crawler_2)
+schedule.every().wednesday.at("02:00").do(crawler_3)
+schedule.every().wednesday.at("06:00").do(crawler_3)
+schedule.every().thursday.at("02:00").do(crawler_1)
+schedule.every().thursday.at("06:00").do(crawler_1)
+schedule.every().friday.at("02:00").do(crawler_2)
+schedule.every().friday.at("06:00").do(crawler_2)
+schedule.every().saturday.at("02:00").do(crawler_3)
+schedule.every().saturday.at("06:00").do(crawler_3)
 
 while True:
     schedule.run_pending()
